d9f1008406c58502fd22241661d356d947f1921e
wanniwa <734933034@qq.com>
2025-02-26 02:00:31 +0800

    update version

--- app/common/setting.py
+++ app/common/setting.py
@@ -8,7 +8,7 @@ DEBUG = "__compiled__" not in globals()
 
 YEAR = 2024
 AUTHOR = "wanniwa"
-VERSION = "2.3.1"
+VERSION = "2.3.2"
 APP_NAME = "transtar"
 HELP_URL = "https://www.nexusmods.com/stardewvalley/mods/20435"
 REPO_URL = "https://github.com/wanniwa/transtar"
